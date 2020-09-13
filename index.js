21a5c0d1a293884c702646bce9e18fcd72efb5bc
Mehul Mohan <mehulmpt@gmail.com>
2020-09-13 19:45:57 +0530

    Lab 1 added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Initialize express server on PORT 1337
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('')
+})
+
+app.listen(process.env.PUBLIC_PORT, () => {
+	console.log('Server started')
+})
