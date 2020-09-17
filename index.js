8b900a98a2d945e1024f8681adb9c9ae0c2cc8f8
Mehul Mohan <mehulmpt@gmail.com>
2020-09-18 02:32:58 +0530

    Lab 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
 
@@ -7,6 +8,17 @@ app.get('/', (req, res) => {
 	res.render('index', { myVariable: 'My name is John!' })
 })
 
+app.post('/', (req, res) => {
+	const db = mongoose.connection.db
+	// insert the record in 'test' collection
+
+	res.json({ ok: 1 })
+})
+
+// Setup your mongodb connection here
+// mongoose.connect(...)
+
+// Wait for mongodb connection before server starts
 app.listen(process.env.PUBLIC_PORT, () => {
 	console.log('Server started')
 })
