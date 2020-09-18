d55ad66a46297dafaaf481fad8d4336b7a47ec25
Mehul Mohan <mehulmpt@gmail.com>
2020-09-18 06:56:23 +0530

    lab

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ app.get('/', (req, res) => {
 
 app.post('/short', async (req, res) => {
 	// Grab the fullUrl parameter from the req.body
+	const fullUrl = req.body.fullUrl
+	console.log('URL requested: ', fullUrl)
 
 	// insert and wait for the record to be inserted using the model
+	const record = new ShortURL({
+		full: fullUrl
+	})
+
+	await record.save()
 
 	res.redirect('/')
 })
